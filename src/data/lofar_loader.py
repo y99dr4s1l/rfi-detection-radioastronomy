5611d862e63b5c11fa616c495d101c557284adb5
Francesco <francesco.nastasi@edu.unito.it>
2026-03-04 19:04:06 +0100

    update loader lofar

--- src/data/lofar_loader.py
+++ src/data/lofar_loader.py
@@ -1,14 +1,15 @@
+import sys
+sys.path.insert(0, '/kaggle/working/RFI-NLN')
 from utils.data import get_lofar_data
 
-##### DEFINE ARGS
+
 class Args:
     def __init__(self):
-        # Following master order
         self.model = 'UNET'
         self.anomaly_class = 1
         self.latent_dim = 32
         self.data = 'LOFAR'
-        self.data_path = '/kaggle/input/lofar-full-rfi-dataset-pkl/'
+        self.data_path = None  # da impostare esternamente
         self.seed = 42
         self.patches = True
         self.patch_x = 32
@@ -16,11 +17,27 @@ class Args:
         self.patch_stride_x = 32
         self.patch_stride_y = 32
         self.input_shape = (512, 512, 1)
+
     def update_input_shape(self):
-        """Aggiorna input_shape basandosi sulle patch"""
         if self.patches:
             self.input_shape = (self.patch_x, self.patch_y, self.input_shape[-1])
 
-# Uso:
-args = Args()
-args.update_input_shape()  # Questo aggiorna input_shape quando serve
+
+def load_lofar(data_path: str, **kwargs) -> tuple:
+    """
+    Loads the LOFAR dataset using RFI-NLN's get_lofar_data.
+
+    Args:
+        data_path: Path to the LOFAR pickle file directory.
+        **kwargs: Optional overrides for Args fields (e.g. patch_x=64).
+
+    Returns:
+        tuple: (train_data, train_masks, test_data, test_masks)
+    """
+    args = Args()
+    args.data_path = data_path
+    for key, value in kwargs.items():
+        setattr(args, key, value)
+    args.update_input_shape()
+
+    return get_lofar_data(args)
